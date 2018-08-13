885e441a7efa991235bbb2ae55720f2b6f3f74fd
Dimitry Fedotov <dimitry.fedotov@ctco.lv>
2018-08-13 11:43:45 +0300

    initial commit

---
+++ src/main/java/lv/ctco/javaschool/firstweb/HelloWorldServlet.java
@@ -0,0 +1,22 @@
+package lv.ctco.javaschool.firstweb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            out.append("Hello, World, from the servlet!");
+        }
+    }
+}
